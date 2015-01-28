84dffde3add45c0a711991bd2deb411570373fac
Travis Beck <tjbenator@gmail.com>
2015-01-27 18:32:37 -0800

    Added Mongojs piece

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 var irc = require('irc');
-
+var mongojs = require('mongojs');
 var config = require('./config.json');
 
-var client = new irc.Client(config.server, config.nick, config.options);
+var db = mongojs(config.db.connectionString);
+var collection = db.collection(config.db.collection);
 
-client.addListener('message', function (from, to, message) {
-    console.log(from + ' => ' + to + ': ' + message);
+//Drop documents older than our set retention time
+collection.ensureIndex({ "createdAt": 1 }, { expireAfterSeconds: config.db.retention })
+
+//Create IRC Client
+var client = new irc.Client(config.irc.server, config.irc.nick, config.irc.options);
+
+var logSave = function(hash) {
+	hash.createdAt = new Date;
+	collection.save(hash, {w: 0});
+}
+
+//Listeners
+client.addListener('message', function (nick, channel, message) {
+    logSave({channel: channel, type: 'message', nick: nick, message: message});
+});
+
+client.addListener('join', function (channel, nick, message) {
+    logSave({channel: channel, type: 'join', nick: nick});
+});
+
+client.addListener('part', function (channel, nick, reason, message) {
+    logSave({channel: channel, type: 'part', nick: nick, reason: reason});
 });
 
 client.addListener('error', function(message) {
