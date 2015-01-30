8e2ee2f2ab66dcc5bf9c55fded26ad339e24f7a7
Travis Beck <tjbenator@gmail.com>
2015-01-30 03:02:01 +0000

    Added a few commands and the ability to add channels on the fly

--- main.js
+++ main.js
@@ -3,22 +3,52 @@ var mongojs = require('mongojs');
 var config = require('./config.json');
 
 var db = mongojs(config.db.connectionString);
-var collection = db.collection(config.db.collection);
+var chat = db.collection(config.db.chat);
+var channels = db.collection(config.db.channels);
 
-//Drop documents older than our set retention time
-collection.ensureIndex({ "createdAt": 1 }, { expireAfterSeconds: config.db.retention })
+//Drop documents older than our set retention time (If defined)
+if (typeof config.db.retention !== 'undefined') {
+	chat.ensureIndex({ "Date": 1 }, { expireAfterSeconds: config.db.retention });
+}
+
+//Append channels from database
+channels.find().forEach(function(err, doc) {
+    if (!doc) {
+        return;
+    }
+    config.irc.options.channels.push(doc.channel);
+});
 
 //Create IRC Client
 var client = new irc.Client(config.irc.server, config.irc.nick, config.irc.options);
 
+/** Functions **/
 var logSave = function(hash) {
-	hash.createdAt = new Date;
-	collection.save(hash, {w: 0});
+    hash.Date = new Date;
+    chat.save(hash, {w: 0});
 }
 
-//Listeners
-client.addListener('message', function (nick, channel, message) {
+var channelPersist = function(channel, nick) {
+    client.join(channel);
+    channels.save({channel: channel, nick: nick});
+}
+
+/** Listeners **/
+
+//Channel listener
+client.addListener('message#', function (nick, channel, message) {
     logSave({channel: channel, type: 'message', nick: nick, message: message});
+});
+
+//We are listening for commands here
+client.addListener('message', function (nick, channel, message) {
+    msg = message.split(" ");
+    if (msg[0] === "persist") {
+        channelPersist(msg[1], nick);
+    }
+    if (message === "persistent channels") {
+        client.say(channel, config.irc.options.channels);
+    }
 });
 
 client.addListener('join', function (channel, nick, message) {
